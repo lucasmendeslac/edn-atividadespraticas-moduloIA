4470ce8cc0a72611396cb3e6bc727f67038dcf53
Lucas Mendes Lacerda <159518146+lucasmendeslac@users.noreply.github.com>
2025-06-18 11:50:25 -0300

    Adiciona solução para Atividade 1
    Esse commit inclui a resposta para Atividade 1.

---
+++ atividade01.py
@@ -0,0 +1,45 @@
+# Aluno: Lucas Mendes Lacerda
+# Turma: C3 MAR - BRSA0 186
+
+# Atividade 1 
+print("Hello, world!")
+
+
+# Atividade 2
+numero1 = 12
+numero2 = 14
+
+print(f"Resultado: {numero1 + numero2}")
+
+
+# Atividade 3
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"Volume = {volume} cm³")
+
+
+# Atividade 4
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print(f"Nome do produto: {nome_produto} \n"
+      f"Preço unitário: R${preco_unitario:.2f}\n"
+      f"Quantidade: {quantidade}\n"
+      f"Preço total: R${preco_total:.2f}")
+
+
+# Atividade 5
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+diferenca = A  * B - C * D 
+print(f"DIFERENCA = {diferenca}")
+
+
+
+
+
